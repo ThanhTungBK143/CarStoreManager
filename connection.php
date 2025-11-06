2200cec14d4800c6c1fdda5a0007ea0611f75cf8
topmow <ngon74079@gmail.com>
2025-11-06 23:42:10 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,3 @@
 <?php
-//creating a database connection - $link is a variable use for just connection class
 $link=mysqli_connect("localhost","root","") or die(mysqli_error($link));
 mysqli_select_db($link,"cardealer") or die(mysqli_error($link));

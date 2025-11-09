5f34438fbbad034f8a6de282c8e3bb1d05e7ad84
ThanhTungBK143 <mathanhtungbk2004@gmail.com>
2025-11-10 01:41:53 +0700

    Super Homepage update
    deletecar,deleteuser, logout all in homepage

--- homepage.php
+++ homepage.php
@@ -1,12 +1,45 @@
 <?php
 include "connection.php";
 session_start();
-if(!isset($_SESSION['username'])){
+
+if (!isset($_SESSION['username'])) {
     header('location:login.php');
     exit();
 }
+
 $username = $_SESSION['username'];
+// --- Handle logout ---
+if (isset($_GET['logout'])) {
+    session_unset();
+    session_destroy();
+    header('Location: login.php');
+    exit();
+}
+// === DELETE CAR FUNCTION ===
+if (isset($_GET['delete_car'])) {
+    $id = intval($_GET['delete_car']);
+    $query = "DELETE FROM cars WHERE product_id = $id";
+    if (mysqli_query($link, $query)) {
+        echo "<script>alert('Car deleted successfully!'); window.location='homepage.php';</script>";
+    } else {
+        echo "<script>alert('Error deleting car.'); window.location='homepage.php';</script>";
+    }
+    exit();
+}
+
+// === DELETE USER FUNCTION ===
+if (isset($_GET['delete_user'])) {
+    $id = intval($_GET['delete_user']);
+    $query = "DELETE FROM users WHERE id = $id";
+    if (mysqli_query($link, $query)) {
+        echo "<script>alert('User deleted successfully!'); window.location='homepage.php';</script>";
+    } else {
+        echo "<script>alert('Error deleting user.'); window.location='homepage.php';</script>";
+    }
+    exit();
+}
 ?>
+
 <html lang="en" xmlns="">
 <head>
     <title>Store Manage</title>
@@ -27,11 +60,10 @@ $username = $_SESSION['username'];
         onclick="return confirm('Do you want to add new car?');"
         >NEW CAR</a>
         <a 
-        href="logout.php"
-        class="btn btn-danger logout-btn" 
-        style="position: absolute; top: 0; right: -350px;" 
-        onclick="return confirm('Are you sure you want to log out?');"
-        >LOGOUT</a>
+        href="homepage.php?logout=true" 
+        style="position: absolute; top: 0; right: -350px;"
+        onclick="return confirm('Are you sure you want to log out?');" 
+        class="btn btn-danger">Logout</a>
     </div>   
 </div>
 <!-- new column inserted for records -->
@@ -48,31 +80,31 @@ $username = $_SESSION['username'];
             <th>Color</th>
             <th>Quantity</th>
             <th>Price</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <th></th>
+            <th></th>
         </tr>
         </thead>
         <tbody>
-        <!-- Database connection -->
         <?php
         if (!empty($link)) {
-            $res=mysqli_query($link,"select * from cars");
+            $res = mysqli_query($link, "SELECT * FROM cars");
         }
-        while($row=mysqli_fetch_array($res))
-        {
+
+        while ($row = mysqli_fetch_array($res)) {
             echo "<tr>";
-            echo "<td>"; echo $row["product_id"]; echo "</td>";
-            echo "<td>"; echo $row["make"]; echo "</td>";
-            echo "<td>"; echo $row["model"]; echo "</td>";
-            echo "<td>"; echo $row["year"]; echo "</td>";
-            echo "<td>"; echo $row["color"]; echo "</td>";
-            echo "<td>"; echo $row["quantity"]; echo "</td>";
-            echo "<td>"; echo $row["price"]; echo "</td>";
-            echo "<td>"; ?> <a href="editcar.php?id=<?php echo $row["product_id"]; ?>"><button type="button" class="btn btn-success">Edit </button></a> <?php echo "</td>";
-            echo "<td> <a href='deletecar.php?id={$row['product_id']}'>
-                <button type='button' class='btn btn-danger' onclick=\"return confirm('Are you sure you want to delete this car?');\">Delete</button>
-                </a>
-            </td>";
+            echo "<td>{$row['product_id']}</td>";
+            echo "<td>{$row['make']}</td>";
+            echo "<td>{$row['model']}</td>";
+            echo "<td>{$row['year']}</td>";
+            echo "<td>{$row['color']}</td>";
+            echo "<td>{$row['quantity']}</td>";
+            echo "<td>{$row['price']}</td>";
+            echo "<td><a href='editcar.php?id={$row['product_id']}' class='btn btn-success'>Edit</a></td>";
+            echo "<td><a 
+                href='homepage.php?delete_car={$row['product_id']}'
+                onclick=\"return confirm('Are you sure you want to delete this car?');\"
+                class='btn btn-danger'>Delete</a>
+                </td>";
             echo "</tr>";
         }
         ?>
@@ -88,8 +120,8 @@ $username = $_SESSION['username'];
             <th>Username</th>
             <th>Email</th>
             <th>Phone</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <th></th>
+            <th></th>
         </tr>
         </thead>
         <tbody>
@@ -106,10 +138,10 @@ $username = $_SESSION['username'];
             echo "<td>"; echo $row["email"]; echo "</td>";
             echo "<td>"; echo $row["phone"]; echo "</td>";
             echo "<td>"; ?> <a href="edituser.php?id=<?php echo $row["id"]; ?>"><button type="button" class="btn btn-success">Edit </button></a> <?php echo "</td>";
-            echo "<td>
-                <a href='deleteuser.php?id={$row['id']}'>
-                <button type='button' class='btn btn-danger' onclick=\"return confirm('Are you sure you want to delete this user?');\">Delete</button>
-                </a>
+            echo "<td><a 
+                href='homepage.php?delete_user={$row['id']}'
+                onclick=\"return confirm('Are you sure you want to delete this user?');\"
+                class='btn btn-danger'>Delete</a>
             </td>";
             echo "</tr>";
         }
